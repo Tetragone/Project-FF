d6cd32940ab0519af72006578644bd5d0ac0450d
Tetragone <71478136+Tetragone@users.noreply.github.com>
2024-08-07 16:23:04 +0900

    타이틀 씬 추가

--- Assets/Scripts/Scene/SceneTitle.cs
+++ Assets/Scripts/Scene/SceneTitle.cs
@@ -6,34 +6,14 @@ using UnityEngine.UI;
 
 public class SceneTitle : MonoBehaviour
 {
-    public UI_Loading Loading;
-    public Button ButtonStart;
-
-    private float _percent = 0f;
     private LoadingState _state = LoadingState.start;
     private int _frame = 0;
 
-    private void Awake()
-    {
-        ButtonStart.gameObject.SetActive(false);
-        ButtonStart.onClick.AddListener(() =>
-        {
-            SceneManager.LoadScene("Lobby");
-        });
-    }
-
     private void Update()
     {
-        Loading.SetPercent(_percent);
-        _percent += Time.deltaTime;
         switch (_state)
         {
             case LoadingState.start:
-                if (_percent > 0.1f)
-                {
-                    _percent = 0.1f;
-                }
-
                 if (TableMgr.IsReaded)
                 {
                     _state = LoadingState.loading_data;
@@ -42,17 +22,11 @@ public class SceneTitle : MonoBehaviour
                 break;
 
             case LoadingState.loading_data:
-                if (_percent < 0.7f)
-                {
-                    _percent += 0.1f;
-                }
-                else
-                {
-                    _percent = 0.7f;
-                }
 
                 if (_frame == 0)
                 {
+                    _state = LoadingState.done;
+                    _frame = -1;
                     //UserDataMgr.Instance.LoadData();
                 }
 
@@ -64,9 +38,7 @@ public class SceneTitle : MonoBehaviour
                 break;
 
             case LoadingState.done:
-                _percent = 1f;
-                ButtonStart.gameObject.SetActive(true);
-                Loading.gameObject.SetActive(false);
+                SceneManager.LoadScene("Game");
                 break;
         }
 
