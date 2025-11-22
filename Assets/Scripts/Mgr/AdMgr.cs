c72e3c44549afc5a96f3a3df2bf9dae6cd6ec0b1
Tetragone <71478136+Tetragone@users.noreply.github.com>
2025-11-23 01:04:46 +0900

    작업 저장

--- Assets/Scripts/Mgr/AdMgr.cs
+++ Assets/Scripts/Mgr/AdMgr.cs
@@ -2,23 +2,32 @@ using GoogleMobileAds.Api;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class AdMgr : SingletonAllSecen<AdMgr>
 {
     private RewardAd Rewarded;
-    private string RewardId = "";
+    private string RewardId = "ca-app-pub-3940256099942544/5224354917";
 
     protected override void SetDataInAwake()
     {
         AdIDs ids = Resources.Load<AdIDs>("AdIds");
 
+        if (ids != null)
+        {
 #if UNITY_ANDROID
-        RewardId = ids.AndoridRewardId;
+            RewardId = ids.AndoridRewardId;
 #elif UNITY_IOS
-        RewardId = ids.IOSRewardId;
+            RewardId = ids.IOSRewardId;
 #endif
+        }
 
         Rewarded = new RewardAd(RewardId);
+    }
+
+    public void SetRewardAction(UnityAction successAction, UnityAction failAction)
+    {
+        Rewarded.SetAction(successAction, failAction);
     }
 
     public void ShowRewarded()
@@ -28,5 +37,11 @@ public class AdMgr : SingletonAllSecen<AdMgr>
             // 호출이 안된 경우도 필요함
             Rewarded.Show();
         }
+    }
+
+
+    // reloading 기달리는것을 구현해야함
+    private void Update()
+    {
     }
 }
--- Assets/Scripts/Module/RewardAd.cs
+++ Assets/Scripts/Module/RewardAd.cs
@@ -1,61 +1,96 @@
 using UnityEngine;
+using GoogleMobileAds;
 using GoogleMobileAds.Api;
 using System;
+using UnityEngine.Events;
 
 public class RewardAd
 {
+    public bool IsLoaded 
+    { 
+        get
+        {
+            return rewarded != null && rewarded.CanShowAd();
+        }
+    }
+
+    public bool IsLoading = false;
+
     private RewardedAd rewarded;
     private string adUnitId;
 
-    public bool IsLoaded = false;
+    private int Counter = 0;
+    private UnityAction SuccessAction;
+    private UnityAction FailAction;
 
     public RewardAd(string adId)
     {
         adUnitId = adId;
-        LoadRewarded();
+        MobileAds.Initialize((initStatue) => 
+        {
+            LoadRewarded();
+        });
     }
 
-    public void LoadRewarded()
+    public bool LoadRewarded()
     {
-        //rewarded = new RewardedAd(adUnitId);
+        IsLoading = true;
 
-        //rewarded.OnAdLoaded += (sender, args) =>
-        //{
-        //    Debug.Log("[AdMob] Rewarded loaded");
-        //};
-        //rewarded.OnAdFailedToLoad += (sender, args) =>
-        //{
-        //    Debug.LogWarning("[AdMob] Rewarded load failed: " + args.LoadAdError);
-        //};
-        //rewarded.OnAdFailedToShow += (sender, args) =>
-        //{
-        //    Debug.LogWarning("[AdMob] Rewarded show failed: " + args.AdError);
-        //};
-        //rewarded.OnAdClosed += (sender, args) =>
-        //{
-        //    Debug.Log("[AdMob] Rewarded closed → reload");
-        //    LoadRewarded();
-        //};
-        //rewarded.OnUserEarnedReward += (sender, reward) =>
-        //{
-        //    Debug.Log($"[AdMob] Rewarded! type={reward.Type}, amount={reward.Amount}");
-        //    // 여기에서 보상 지급
-        //    // GameManager.Instance.AddGem((int)reward.Amount);
-        //};
+        if (Counter > 10)
+        {
+            return false;
+        }
 
-        //var request = new AdRequest.Builder().Build();
-        //rewarded.LoadAd(request);
+        if (Application.internetReachability == NetworkReachability.NotReachable)
+        {
+            return false;
+        }
+
+        var adRequest = new AdRequest();
+        /*
+        // 경고: 광고를 로드하지 못했을 때 광고 요청 완료 블록에서 새 광고 로드를 시도하지 마세요.
+        // 광고 요청 완료 블록에서 광고를 로드해야 하는 경우에는 네트워크 연결이 제한된 상황 등에서
+        // 광고 요청이 계속 실패하지 않도록 광고 로드 재시도 횟수를 제한하세요.
+        라고 써져 있었으니, 이것을 고려해서 작성하자.
+        */
+
+        RewardedAd.Load(adUnitId, adRequest, (ad, error) =>
+        {
+            IsLoading = false;
+
+            if (error != null)
+            {
+                Counter++;
+                return;
+            }
+
+            Counter = 0;
+            rewarded = ad;
+        });
+
+        return true;
+    }
+
+    public void SetAction(UnityAction successAction, UnityAction failAction)
+    {
+        SuccessAction = successAction;
+        FailAction = failAction;
     }
 
     public void Show()
     {
-        //if (rewarded != null && rewarded.IsLoaded())
-        //{
-        //    rewarded.Show();
-        //}
-        //else
-        //{
-        //    Debug.Log("[AdMob] Rewarded not ready");
-        //}
+        if (IsLoaded)
+        {
+            rewarded.Show((reward) =>
+            {
+                LoadRewarded();
+                SuccessAction.Invoke();
+            });
+        }
+        else
+        {
+            Debug.Log("[AdMob] Rewarded not ready");
+            FailAction.Invoke();
+        }
     }
 }
